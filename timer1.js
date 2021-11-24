9e94344e65bc7ac9c2d8c9a66078e8c0ee960ec0
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-23 21:42:05 -0800

    Remove unnecessary index parameter

--- timer1.js
+++ timer1.js
@@ -7,7 +7,7 @@ if (!input) {
 let goodInput = input.filter(num => !isNaN(num));
 goodInput = goodInput.filter(num => num >= 0);
 
-goodInput.forEach((num, index) => {
+goodInput.forEach((num) => {
     num *= 1000;
     setTimeout(() => {
       process.stdout.write('\x07');
